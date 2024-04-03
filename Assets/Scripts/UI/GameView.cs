458b6f3b1368eede1e35f16343f69cbed73de86c
EmilGlz <emilquluzade2001@gmail.com>
2024-04-03 15:44:23 -0400

    Showing FPS in game view

--- Assets/Scripts/UI/GameView.cs
+++ Assets/Scripts/UI/GameView.cs
@@ -1,17 +1,34 @@
 using Assets.Scripts.UI;
+using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Assets.Scripts
 {
     public class GameView : View
     {
+        public TMP_Text fpsText;
+        float deltaTime = 0.0f;
+        private bool _canCheckFPS;
+
         void Awake()
         {
             InitButtons();
         }
 
+        public override void Enter()
+        {
+            _canCheckFPS = true;
+        }
+
+        public override void Exit()
+        {
+            _canCheckFPS = false;
+        }
+
         private void InitButtons()
         {
+            fpsText = Utils.FindGameObject<TMP_Text>("FPSText", transform);
             var backButton = Utils.FindGameObject<Button>("BackButton", transform);
             backButton.onClick.RemoveAllListeners();
             backButton.onClick.AddListener(() =>
@@ -19,6 +36,18 @@ namespace Assets.Scripts
                 Main.Instance.EnterView<FractallScrollView>();
                 Main.Instance.LoadScene(0);
             });
+        }
+
+        void Update()
+        {
+            if (!_canCheckFPS)
+                return;
+            deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+            float msec = deltaTime * 1000.0f;
+            float fps = 1.0f / deltaTime;
+            string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+            if (fpsText != null)
+                fpsText.text = text;
         }
     }
 }
